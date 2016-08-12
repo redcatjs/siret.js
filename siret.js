063fd711afeab178bec948affe230adf0c7abc63
jo <jo@surikat.pro>
2016-08-12 11:18:51 +0200

    improve by handle string and auto trim

--- siret.js
+++ siret.js
@@ -1,5 +1,8 @@
 (function(w){
 	w.checkLuhn = function(val){
+		if(typeof(val)=='string'){
+			val = parseInt(val,10);
+		}
 		var len = val.length;
 		var total = 0;
 		for(var i = 1; i <= len; i++){
@@ -15,15 +18,22 @@
 		return !!(total % 10 == 0);
 	};
 	w.checkSiret = function(siret) {
-		return w.checkLuhn(siret);
+		siret = siret.replace(/ /g, '');
+		return siret.length==14&&w.checkLuhn(siret);
 	};
 	w.checkSiren = function(siren) {
-		return w.checkLuhn(siren);
+		siren = siren.replace(/ /g, '');
+		return siren.length==9&&w.checkLuhn(siren);
 	};
 	w.siret2siren = function(siret) {
+		siret = siret.replace(/ /g, '');
 		return siret.substr(0,9);
 	};
 	w.siren2tvaFR = function(siren) {
+		siren = siren.replace(/ /g, '');
+		if(typeof(siren)=='string'){
+			siren = parseInt(siren,10);
+		}
 		return "FR" + (( 12 + 3 * ( siren % 97 ) ) % 97 ) + siren;
 	};
 })(window);
